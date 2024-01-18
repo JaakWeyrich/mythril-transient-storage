0ea3151c659c9cce63e72aa98e880271574b0c03
Jaak Weyrich <jaak@OMEGALAPTOP-3000.org>
2024-01-18 17:46:04 +0100

    implemented model for transient storage itself

---
+++ mythril/laser/ethereum/state/transient_storage.py
@@ -0,0 +1,49 @@
+"""This module contains a representation of a smart contract's transient storage."""
+import logging
+from copy import deepcopy
+from typing import Any, Dict
+
+
+from mythril.laser.smt import BitVec, simplify, If, Bool
+
+log = logging.getLogger(__name__)
+
+
+class TransientStorage:
+    """A class representing contract transient storage"""
+
+    def __init__(self, concrete=False) -> None:
+        """Constructor for transient storage."""
+        
+        self._transientStorage: Dict[BitVec, BitVec] = {}
+
+    def __getitem__(self, item: BitVec) -> BitVec:
+        """
+        Access a word from a specified transient storage index.
+
+        :param index: integer representing the word index to access
+        :return: 256-bit word at the specified index
+        """
+
+        transientStorage = self._transientStorage
+        return simplify(transientStorage[item])
+
+    def __setitem__(self, key, value: Any) -> None:
+        if isinstance(value, Bool):
+            value = If(value, 1, 0)
+
+        self._transientStorage[key] = value
+
+    def __deepcopy__(self, memodict=dict()):
+        """always treat uninitialized values as false for transient storage for now, see how tests perform"""
+        concrete = False
+        transientStorage = TransientStorage(
+            concrete=concrete
+        )
+        """deepcopy (like storage) instead of copy like for memory, see how tests perform"""
+        transientStorage._transientStorage = deepcopy(self._transientStorage)
+        return transientStorage
+
+    def __str__(self) -> str:
+        # TODO: Do something better here
+        return str(self._transientStorage)
