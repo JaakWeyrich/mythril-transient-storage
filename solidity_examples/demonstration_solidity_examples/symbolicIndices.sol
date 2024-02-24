196f0791859ee7bf8d31b8601e9149493ecfab14
Jaak Weyrich <jaak@OMEGALAPTOP-3000.org>
2024-02-24 11:54:15 +0100

    added demonstration contract to demonstrate that transient storage fails to recognize the potential equivalence of different symbolic indices.

---
+++ solidity_examples/demonstration_solidity_examples/symbolicIndices.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// This demonstration contract demonstrates that transient storage fails to recognize the potential equivalene of two different symbolic indices.
+
+contract myContract {
+
+    constructor() payable {}
+
+    function arbitraryWrite(uint256 _key1, uint256 _key2) public {
+        assembly {
+            sstore(_key1, 123)
+
+            // if transient storage at key2 is equal to what has been stored at key1, send balance of contract to msg.sender
+            if eq(sload(_key2), 123) {
+                let success := call(gas(), caller(), selfbalance(), 0, 0, 0, 0)
+            }
+        }
+    }
+}
